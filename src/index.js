f9f274f40a46b9f9cbbc7c6283d56a1dbfe73f5b
irina-sukorskaya <69984819+irina-sukorskaya@users.noreply.github.com>
2020-10-25 15:39:11 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-export default function reverse (n) {
-    var rev = "";
-    number = parseInt (n);
-    for (;number > 0;) {
-        rev += number % 10;
-        number = Math.floor (number / 10)
-    }
-    return rev;
+module.exports = function reverse (n) {
+    return (
+        parseFloat (
+        n
+        .toString ()
+        .split('')
+        .reverse ()
+        .join ('')
+    ) * Math.sign (n)
+    )
+  
 }
