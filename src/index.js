fbe56dcdecd84f3417d3f7f4db5b8109c74c36d5
irina-sukorskaya <69984819+irina-sukorskaya@users.noreply.github.com>
2020-10-25 15:28:56 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+export default function reverse (n) {
+    var rev = "";
+    number = parseInt (n);
+    for (;number > 0;) {
+        rev += number % 10;
+        number = Math.floor (number / 10)
+    }
+    return rev;
 }
